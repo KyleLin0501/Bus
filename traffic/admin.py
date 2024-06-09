62ebb9c2909c7306eba8277dac2846a4fb3f8093
KyleLin0501 <kylelin930501@gmail.com>
2024-06-09 14:07:36 +0800

    0609update

--- traffic/admin.py
+++ traffic/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from .models import Bus
 from .models import HistoricalArrivals
+from .models import ActualFrequency
+from .models import Route
+from .models import AppointmentForm
+from .models import Dirver
+from .models import Passenger
+from .models import Schedules
+from .models import Station
+from .models import VehicleMaintenanceRecords
+
+
+
 admin.site.register(Bus)
 admin.site.register(HistoricalArrivals)
+admin.site.register(ActualFrequency)
+admin.site.register(Route)
+admin.site.register(AppointmentForm)
+admin.site.register(Dirver)
+admin.site.register(Passenger)
+admin.site.register(Schedules)
+admin.site.register(Station)
+admin.site.register(VehicleMaintenanceRecords)
+
 
 # Register your models here.
--- traffic/views.py
+++ traffic/views.py
@@ -15,9 +15,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    # 从数据库中获取所有 Bus 对象的列表
     bus_list = Bus.objects.all()
-    # 将 bus_list 传递给模板
     return render(request, "index.html", {"bus_list": bus_list})
 
 def search(request):
@@ -39,6 +37,67 @@ def search(request):
         'outbound_returns': HistoricalArrivals.objects.values_list('outbound_return', flat=True).distinct(),
         'route_numbers': HistoricalArrivals.objects.values_list('route_number', flat=True).distinct(),
     })
+
+
+# # views.py
+# from django.shortcuts import render
+# from .models import ActualFrequency, Route
+# from .forms import SearchForm
+#
+#
+# def search(request):
+#     form = SearchForm(request.GET or None)
+#     results = []
+#
+#     if form.is_valid():
+#         driving_date = form.cleaned_data.get('driving_date')
+#         jurisdiction_unit = form.cleaned_data.get('jurisdiction_unit')
+#         route_number = form.cleaned_data.get('route_number')
+#         outbound_return = form.cleaned_data.get('outbound_return')
+#
+#         # Query the ActualFrequency model
+#         frequencies = ActualFrequency.objects.all()
+#
+#         if driving_date:
+#             frequencies = frequencies.filter(driving_date=driving_date)
+#         if jurisdiction_unit:
+#             frequencies = frequencies.filter(jurisdiction_unit=jurisdiction_unit)
+#         if route_number:
+#             frequencies = frequencies.filter(route_number=route_number)
+#         if outbound_return:
+#             frequencies = frequencies.filter(outbound_return=outbound_return)
+#
+#         # Combine data from Route model
+#         for freq in frequencies:
+#             route = Route.objects.filter(
+#                 jurisdiction_unit=freq.jurisdiction_unit,
+#                 route_number=freq.route_number,
+#                 outbound_return=freq.outbound_return
+#             ).first()
+#
+#             if route:
+#                 results.append({
+#                     'driving_date': freq.driving_date,
+#                     'departure_time': freq.departure_time,
+#                     'driving_week': freq.driving_week,
+#                     'jurisdiction_unit': freq.jurisdiction_unit,
+#                     'route_number': freq.route_number,
+#                     'outbound_return': freq.outbound_return,
+#                     'starting_point_x': route.starting_point_x,
+#                     'starting_point_y': route.starting_point_y,
+#                     'destination_x': route.destination_x,
+#                     'destination_y': route.destination_y,
+#                     'driver_id': freq.driver_id,
+#                     'vehicle_license_plate': freq.vehicle_license_plate,
+#                 })
+#
+#     context = {
+#         'form': form,
+#         'results': results
+#     }
+#
+#     return render(request, 'search.html', context)
+
 
 def lover(request):
     filters = {}
